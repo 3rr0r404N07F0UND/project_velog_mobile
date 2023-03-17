6f516ab90569a74e3f35856f2d0501341b6b8623
SIA <gundam3331@naver.com>
2023-03-17 16:48:04 +0900

    project mobile velog mobile new
    project mobile velog mobile new

---
+++ src/js/main.dev.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var counter1 = 0;
+var languageArray = [
+/*"XHTML",*/
+"CSS", "JavaScript", "TypeScript", "jQuery", "jQuery UI", "jQuery Mobile", "SCSS", "SASS", "Stylus", "PUG", "Nunjucks", "MySQL", "MariaDB", "PostgreSQL", "Java", "JSON", "Node.js", "Python", "Ruby", "C", "C++", "R", "SVG", "HTML"];
+
+var loopArray = function loopArray() {
+  setTimeout(function () {
+    if (counter1 == languageArray.length) {
+      counter1 = 0;
+      loopArray();
+    } else {
+      var i = 0;
+      var rememberLoopWord = "";
+
+      var wordLoopFunction = function wordLoopFunction() {
+        if (i < languageArray[counter1].length) {
+          setTimeout(function () {
+            rememberLoopWord = rememberLoopWord + languageArray[counter1].charAt(i);
+            $("h2 > p > span").html(rememberLoopWord);
+            i++;
+            wordLoopFunction();
+          }, 80);
+        } else {
+          counter1++;
+          loopArray();
+        }
+      };
+
+      wordLoopFunction();
+    }
+  }, 500);
+};
+
+loopArray();
+var vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+window.addEventListener("resize", function () {
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+});
+window.addEventListener("touchend", function () {
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
+});
+$(".portfolioIcon").on("click", function () {
+  $(".portfolioInnerBox").addClass("fadeOutAnimation");
+});
+$(".notPortfolio").on("click", function () {
+  console.log("click");
+  $(".portfolioInnerBox").removeClass("fadeOutAnimation");
+});
---
+++ src/js/main.js
@@ -0,0 +1,72 @@
+let counter1 = 0;
+const languageArray = [
+  /*"XHTML",*/
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "jQuery",
+  "jQuery UI",
+  "jQuery Mobile",
+  "SCSS",
+  "SASS",
+  "Stylus",
+  "PUG",
+  "Nunjucks",
+  "MySQL",
+  "MariaDB",
+  "PostgreSQL",
+  "Java",
+  "JSON",
+  "Node.js",
+  "Python",
+  "Ruby",
+  "C",
+  "C++",
+  "R",
+  "SVG",
+  "HTML",
+];
+let loopArray = () => {
+  setTimeout(() => {
+    if (counter1 == languageArray.length) {
+      counter1 = 0;
+      loopArray();
+    } else {
+      let i = 0;
+      let rememberLoopWord = "";
+      let wordLoopFunction = () => {
+        if (i < languageArray[counter1].length) {
+          setTimeout(() => {
+            rememberLoopWord =
+              rememberLoopWord + languageArray[counter1].charAt(i);
+            $("h2 > p > span").html(rememberLoopWord);
+            i++;
+            wordLoopFunction();
+          }, 80);
+        } else {
+          counter1++;
+          loopArray();
+        }
+      };
+      wordLoopFunction();
+    }
+  }, 500);
+};
+loopArray();
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+window.addEventListener("touchend", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+$(".portfolioIcon").on("click", () => {
+  $(".portfolioInnerBox").addClass("fadeOutAnimation");
+});
+$(".notPortfolio").on("click", () => {
+  console.log("click");
+  $(".portfolioInnerBox").removeClass("fadeOutAnimation");
+});
